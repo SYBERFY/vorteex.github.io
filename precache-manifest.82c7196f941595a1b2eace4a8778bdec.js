a353960d578cf3b36162dfd6f08012aef9cd9bc0
Adrian Dinis <adriandinis99@gmail.com>
2020-11-30 05:13:22 +0000

    Build website

---
+++ precache-manifest.82c7196f941595a1b2eace4a8778bdec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b65480bf896d5e629d79",
+    "url": "/static/css/main.c31ddbe8.chunk.css"
+  },
+  {
+    "revision": "b65480bf896d5e629d79",
+    "url": "/static/js/main.b65480bf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "a2b64f4b65170a3d9077",
+    "url": "/static/css/2.ee3d0b0f.chunk.css"
+  },
+  {
+    "revision": "a2b64f4b65170a3d9077",
+    "url": "/static/js/2.a2b64f4b.chunk.js"
+  },
+  {
+    "revision": "c24fdf1f156c66bfb4ee850a599a4c64",
+    "url": "/static/media/logo-white.c24fdf1f.svg"
+  },
+  {
+    "revision": "3c9d0aee7b63d81a3bf1d55c81beb501",
+    "url": "/index.html"
+  }
+];
